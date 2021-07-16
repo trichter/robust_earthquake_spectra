34dde584573bc8f78b44c719ced973ddc4cd8646
trichter <tom.eulenfeld@uni-jena.de>
2021-07-16 14:48:05 +0200

    add grond folder

---
+++ grond/run_grond.sh
@@ -0,0 +1,5 @@
+grond check swarms.gronf x201810202 x201816718 x201820961 x201825814 x201845973 x201855981 x201856087 x201869746 x201872635
+grond go swarms.gronf all
+grond report runs/cmt_*.grun
+grond export mean runs/cmt_*.grun > solutions_mean.txt
+grond export best runs/cmt_*.grun > solutions_best.txt
