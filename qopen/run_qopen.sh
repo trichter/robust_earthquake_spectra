a0773b7234f5d5d2c836185d574b7c3de6580602
trichter <tom.eulenfeld@uni-jena.de>
2021-10-06 17:16:37 +0200

    add qopen command to produce example optimization plots

--- qopen/run_qopen.sh
+++ qopen/run_qopen.sh
@@ -3,3 +3,4 @@ qopen fixed --input "01_go/results.json" --align-sites --prefix "02_sites/"
 qopen source --input "01_go/results.json" --input-sites "02_sites/results.json" --prefix "03_mag/" --overwrite-conf '{"skip": {"coda_window": 1, "num_pairs": 2}}'
 qopen recalc_source --input "03_mag/results.json" --prefix "04_mag_nconst/" --seismic-moment-options '{"fc": null, "n": 1.88, "gamma": 2, "num_points": 4}'
 qopen go -e 20186784 --dump-fitpkl 01_go/fits_%s.pkl --no-plots --output null
+qopen go -e 20186784 --prefix 01_go_20186784/ --plot-optimization --overwrite-conf '{"g0_bounds": [1e-6, 1e-4]}'
